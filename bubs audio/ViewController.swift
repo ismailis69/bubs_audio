70ad3fd5145feb964e5f6a5198678794fe4880ea
Steven Ismaili <srijr05@gmail.com>
2020-09-21 17:59:20 -0400

    Initial Commit

---
+++ bubs audio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bubs audio
+//
+//  Created by Steven Ismaili on 9/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ bubs audioTests/bubs_audioTests.swift
@@ -0,0 +1,33 @@
+//
+//  bubs_audioTests.swift
+//  bubs audioTests
+//
+//  Created by Steven Ismaili on 9/21/20.
+//
+
+import XCTest
+@testable import bubs_audio
+
+class bubs_audioTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
